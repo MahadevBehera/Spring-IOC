e1c94903727900df0853376791dd371907057394
MahadevBehera <mahadev.behera@gmail.com>
2019-09-20 00:37:23 +0530

    Committing some comments

--- src/Lab20/Lab20A.java
+++ src/Lab20/Lab20A.java
@@ -3,7 +3,7 @@ package Lab20;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-// Autowiring - ByType Example Using XML Based Configuration (Spring IOC)
+// Autowiring - ByType Example Using Java Based Configuration (Spring IOC)
 // No bean found with matching Data Type for each dependency of Hello Bean, So remains uninjected 
 public class Lab20A {
 	public static void main(String[] args) {
