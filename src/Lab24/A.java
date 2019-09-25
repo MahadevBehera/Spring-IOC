cb19674edb612bf9ff4f3da4772bd481f6407f95
MahadevBehera <mahadev.behera@gmail.com>
2019-09-25 07:28:11 +0530

    Committing Example of Autowiring - constructor (Only one matching type bean found for A type Bean in 23-Lab & B type Bean in 24-Lab) Using XML Bean Config

---
+++ src/Lab24/A.java
@@ -0,0 +1,25 @@
+package Lab24;
+
+public class A {
+	private int a;       // S.I
+	private String msg;  // S.I
+	
+	public A() {
+		System.out.println("A -- D.C");
+	}
+
+	public void setA(int a) {
+		System.out.println("A -- setA()");
+		this.a = a;
+	}
+
+	public void setMsg(String msg) {
+		System.out.println("A -- setMsg()");
+		this.msg = msg;
+	}
+
+	@Override
+	public String toString() {
+		return "A [a=" + a + ", msg=" + msg + "]";
+	}
+}
