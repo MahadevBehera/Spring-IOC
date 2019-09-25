91e88e18fde2aa1981d6774592b8ed76fbb36c79
MahadevBehera <mahadev.behera@gmail.com>
2019-09-25 07:23:28 +0530

    Committing Example of Autowiring - constructor (Exactly one matching type bean found) Using XML Bean Configuration (Spring IOC)

---
+++ src/Lab22/Hello.java
@@ -0,0 +1,32 @@
+package Lab22;
+
+public class Hello {
+	private A aobj; // S.I
+	private B bobj; // S.I
+	
+	public Hello() {
+		System.out.println("Hello-()");
+	}
+	
+	public Hello(A aobj, B bobj) {
+		System.out.println("Hello-(A,B)");
+		this.aobj = aobj;
+		this.bobj = bobj;
+	}
+	
+	public Hello(A aobj) {
+		System.out.println("Hello-(A)");
+		this.aobj = aobj;
+	}
+
+	public Hello(B bobj) {
+		System.out.println("Hello-(B)");
+		this.bobj = bobj;
+	}
+	
+	public void show() {
+		System.out.println("show in Hello");
+		System.out.println(aobj); // A [a=10, msg=AAA]
+		System.out.println(bobj); // B [b=88, str=Hai Guys]
+	}
+}
---
+++ src/Lab22/Lab22A.java
@@ -0,0 +1,17 @@
+package Lab22;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+// Autowiring - constructor Example Using XML Based Configuration (Spring IOC)
+public class Lab22A {
+	public static void main(String[] args) {
+		
+	//	ApplicationContext ctx = new ClassPathXmlApplicationContext(); // It will find the bean configuration XML file which default name "applicationContext.xml"
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("/Lab22/myBeanFile.xml"); // to lead our own custom defined bean configuration XML file
+		System.out.println("Spring Container is Ready");
+		System.out.println("------------------------------------------");
+		Hello h = (Hello)ctx.getBean("hello");
+		h.show();
+	}
+}
